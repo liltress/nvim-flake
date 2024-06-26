997a7bd9deb5ffa6f3a23b0cbfd46f4e402599d1
liltress <138496125+liltress@users.noreply.github.com>
2024-06-26 14:17:34 +0200

    improved options

--- config/options.nix
+++ config/options.nix
@@ -1,9 +1,23 @@
 {self, ...}: {
   config.opts = {
     # TODO clipboard support
+    # line numbers
     number = true;
+    relativenumber = true;
+    # always show sign column
+    signcolumn = "yes";
     autoindent = true;
+    # tab defaults
+    tabstop = 4;
+    shiftwidth = 4;
+    softtabstop = 0;
+    expandtab = true;
     smarttab = true;
-    cursorline = true;    # highlight current line
+    # hightlight current line
+    cursorline = true;    
+    # start scholling this many lines away from the eduge
+    scrolloff = 5;
+    # clipboard (add xlip)
+    clipboard = "unnamedplus";
   };
 }
